82f8fca43496e560b34b4bd12d86d5ae5c17789c
Johan Liljegren <liljegren.johan@gmail.com>
2013-03-02 23:36:55 +0100

    Added codeigniter and bootstrap

---
+++ application/views/footer.php
@@ -0,0 +1,6 @@
+	    <footer>
+	    	&copy; SHAS 2013
+	    </footer>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+	</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,7 @@
+<html>
+	<head>
+		<title>SAHS - Shared Home Administration System</title>
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	</head>
+	<body>
+		
